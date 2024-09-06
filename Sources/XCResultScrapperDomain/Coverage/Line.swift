e7e986f7dbe0942108e7bbda986a0d497fbbb2af
Sergey Petrachkov <petrachkovsergey@gmail.com>
2024-09-06 15:11:51 +0200

    address an issue where the parsing of coverage report would fail because of executionCount being a number bigger than Int64

--- Sources/XCResultScrapperDomain/Coverage/Line.swift
+++ Sources/XCResultScrapperDomain/Coverage/Line.swift
@@ -6,6 +6,19 @@ public struct Line: Decodable {
         public let column: Int
         public let executionCount: Int?
         public let length: Int
+
+        public enum CodingKeys: CodingKey {
+            case column
+            case executionCount
+            case length
+        }
+
+        public init(from decoder: any Decoder) throws {
+            let container: KeyedDecodingContainer<Line.Subrange.CodingKeys> = try decoder.container(keyedBy: Line.Subrange.CodingKeys.self)
+            self.column = try container.decode(Int.self, forKey: Line.Subrange.CodingKeys.column)
+            self.executionCount = try? container.decodeIfPresent(Int.self, forKey: Line.Subrange.CodingKeys.executionCount) ?? 0
+            self.length = try container.decode(Int.self, forKey: Line.Subrange.CodingKeys.length)
+        }
     }
 
     public let line: Int
@@ -15,5 +28,20 @@ public struct Line: Decodable {
 
     public var isCovered: Bool {
         isExecutable && (executionCount ?? 0) > 0
+    }
+
+    public enum CodingKeys: CodingKey {
+        case line
+        case isExecutable
+        case executionCount
+        case subranges
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.line = try container.decode(Int.self, forKey: .line)
+        self.isExecutable = try container.decode(Bool.self, forKey: .isExecutable)
+        self.executionCount = try? container.decodeIfPresent(Int.self, forKey: .executionCount) ?? 0
+        self.subranges = try container.decodeIfPresent([Line.Subrange].self, forKey: .subranges)
     }
 }
